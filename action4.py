e3ece5182848f98ba03dca6ff9e8a24485cec3e1
Quentin Caritey <quentin.caritey@neoxia.com>
2022-02-13 21:02:41 +0100

    action 4 v0

---
+++ action4.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.appName("myproject").master("local").getOrCreate()
+
+full_file = "partial.csv"
+
+full_df = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(full_file)
+
+# Afficher dans la console les 10 mots qui reviennent le plus dans les messages de
+# commit sur l’ensemble des projets.
+full_df.select("repo", "message")\
+    .groupby("message")\
+    .agg(count("message").alias("nbOccurrences")) \
+    .orderBy("nbOccurrences", ascending=False) \
+    .show(n=100)
